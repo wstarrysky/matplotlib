77845d67447264199c8c00cf3f659cdcca77196a
starrysky <wenbo_jiang@163.com>
2020-11-02 22:13:34 +0800

    好像可以了吧

--- 01.基础绘图.py
+++ 01.基础绘图.py
@@ -1,6 +1,6 @@
 from  matplotlib import pyplot as plt
 
-X = range（2，26，2）＃2从26到，步长为2
+x = range(2,26,2)   #2从26到，步长为2
 y=[15 ,13,14.5,17,20,25,26,26,24,22,18,15]#对应的y值
 plt.plot(x,y)#传入x和y的值
 plt.show()#展示坐标图，这不
