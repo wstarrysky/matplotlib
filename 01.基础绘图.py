0dae0a64369ac1aac161c10f4dca1b34cc8e898a
starrysky <wenbo_jiang@163.com>
2020-11-03 15:50:12 +0800

    1

--- 01.基础绘图.py
+++ 01.基础绘图.py
@@ -3,7 +3,5 @@ from  matplotlib import pyplot as plt
 x = range(2,26,2)   #2从26到，步长为2
 y=[15 ,13,14.5,17,20,25,26,26,24,22,18,15]#对应的y值
 plt.plot(x,y)#传入x和y的值
-plt.show()#展示坐标图，这不
-#我就不信了
-#怎么可以这样
-#狂喜
+plt.show()#展示坐标图
+
